8d87df8a4ebc6b582f06ab7fb1f09d14e49fd07f
utkershrajvenshi <utkarshrajvanshi2@gmail.com>
2022-05-20 17:43:22 +0530

    added location-tracking feature for presently logged in user

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:findmyfamily/backend/group_details.dart';
+import 'package:findmyfamily/backend/location_service.dart';
 import 'package:findmyfamily/components/app_icon.dart';
 import 'package:findmyfamily/components/map_bottom_card.dart';
 import 'package:findmyfamily/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -15,6 +19,30 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // State-global controller for Google Map
+  Completer<GoogleMapController> _controller = Completer();
+
+  // Hashmap of all the markers in the map
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+
+  Position getInitialData() {
+    return Position(
+        longitude: 0,
+        latitude: 0,
+        timestamp: DateTime.now(),
+        accuracy: 0,
+        altitude: 0,
+        heading: 0,
+        speed: 0,
+        speedAccuracy: 0);
+  }
+
+  CameraPosition constructCameraPosition(Position? snapshotPosition) =>
+      CameraPosition(
+          target:
+              LatLng(snapshotPosition!.latitude, snapshotPosition.longitude),
+          zoom: 16);
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -44,7 +72,45 @@ class _HomeScreenState extends State<HomeScreen> {
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height,
                 color: Colors.grey.shade200,
-                //child: GoogleMap(initialCameraPosition: ),
+                child: FutureBuilder<Stream<Position>>(
+                    future: LocationService().getLocationStream(),
+                    builder: (context, futureSnapshot) {
+                      if (!futureSnapshot.hasData) {
+                        return Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Text("Loading Map"),
+                              SizedBox(height: 40),
+                              CircularProgressIndicator()
+                            ],
+                          ),
+                        );
+                      }
+                      return StreamBuilder<Position>(
+                        stream: futureSnapshot.data,
+                        initialData: getInitialData(),
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) {
+                            return const CircularProgressIndicator();
+                          }
+                          _animateToUser(snapshot.data);
+                          _addMarker(snapshot.data);
+
+                          return GoogleMap(
+                            initialCameraPosition:
+                                constructCameraPosition(getInitialData()),
+                            myLocationEnabled: true,
+                            onMapCreated: (GoogleMapController controller) {
+                              _controller.complete(controller);
+                            },
+                            markers: markers.values.toSet(),
+                          );
+                        },
+                      );
+                    }
+                ),
+                
               ),
               Align(
                 alignment: Alignment.bottomCenter,
@@ -55,5 +121,23 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
     );
+  }
+
+  Future<void> _animateToUser(Position? myPosition) async {
+    final GoogleMapController mapController = await _controller.future;
+
+    // Animate mapCamera to the location and add a marker at the position
+    mapController.animateCamera(
+        CameraUpdate.newCameraPosition(constructCameraPosition(myPosition)));
+  }
+
+  void _addMarker(Position? myPosition) {
+    MarkerId _markerId = const MarkerId('Lenovo device');
+    Marker marker = Marker(
+      markerId: _markerId,
+      position: LatLng(myPosition!.latitude, myPosition.longitude),
+    );
+
+    markers[_markerId] = marker;
   }
 }

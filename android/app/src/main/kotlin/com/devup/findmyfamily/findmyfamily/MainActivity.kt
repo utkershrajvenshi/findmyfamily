e90abaf3a5e02ab42f09b8364568cb0051f1cfb3
Utkersh Rajvenshi <utkarshrajvanshi2@gmail.com>
2023-10-29 22:46:26 +0530

    Removed flutter code

--- android/app/src/main/kotlin/com/devup/findmyfamily/findmyfamily/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.devup.findmyfamily.findmyfamily
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/backend/id_generator.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:math';
-
-class IDGenerator {
-  static String generateRoomID() {
-    const _charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    var random = Random.secure();
-    return List.generate(6, (index) => _charSet[random.nextInt(_charSet.length)]).join();
-  }
-}
